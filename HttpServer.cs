b3bc97ed1ea4ce521d40d33ee80047649094e69e
MoA <honomoa@gmail.com>
2015-10-31 23:39:43 +0800

    add SimpleHttpServer project

---
+++ HttpServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bend.Util
+{
+    public abstract class HttpServer
+    {
+
+        protected IPAddress server;
+        protected int port;
+        TcpListener listener;
+        bool is_active = true;
+
+        public HttpServer(int port)
+        {
+            this.server = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            this.port = port;
+        }
+
+        public HttpServer(IPAddress server, int port)
+        {
+            this.server = server;
+            this.port = port;
+        }
+
+        public HttpServer(byte[] server, int port)
+        {
+            this.server = new IPAddress(server);
+            this.port = port;
+        }
+
+        public void listen()
+        {
+            listener = new TcpListener(server, port);
+            listener.Start();
+            while (is_active)
+            {
+                TcpClient s = listener.AcceptTcpClient();
+                HttpProcessor processor = new HttpProcessor(s, this);
+                Thread thread = new Thread(new ThreadStart(processor.process));
+                thread.Start();
+                Thread.Sleep(1);
+            }
+        }
+
+        public abstract void handleGETRequest(HttpProcessor p);
+        public abstract void handlePOSTRequest(HttpProcessor p, StreamReader inputData);
+    }
+
+}
---
+++ SimpleHttpServer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+using tw.moa.csharp.MoaLog;
+// offered to the public domain for any use with no restriction
+// and also with no warranty of any kind, please enjoy. - David Jeske. 
+
+// simple HTTP explanation
+// http://www.jmarshall.com/easy/http/
+
+namespace Bend.Util
+{
+
+    public class MyHttpServer : HttpServer
+    {
+        public MyHttpServer(int port)
+            : base(port)
+        {
+        }
+        public override void handleGETRequest(HttpProcessor p)
+        {
+
+            if (p.http_url.Equals("/Test.png"))
+            {
+                Stream fs = File.Open("../../Test.png", FileMode.Open);
+
+                p.writeSuccess("image/png");
+                fs.CopyTo(p.outputStream.BaseStream);
+                p.outputStream.BaseStream.Flush();
+            }
+
+            MoaLog.Debug(String.Format("request: {0}", p.http_url));
+            p.writeSuccess();
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("Current Time: " + DateTime.Now.ToString());
+            p.outputStream.WriteLine("url : {0}", p.http_url);
+
+            p.outputStream.WriteLine("<form method=post action=/form>");
+            p.outputStream.WriteLine("<input type=text name=foo value=foovalue>");
+            p.outputStream.WriteLine("<input type=submit name=bar value=barvalue>");
+            p.outputStream.WriteLine("</form>");
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+            MoaLog.Debug(String.Format("POST request: {0}", p.http_url));
+            string data = inputData.ReadToEnd();
+
+            p.writeSuccess();
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("<a href=/test>return</a><p>");
+            p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+
+
+        }
+    }
+
+    public class TestMain
+    {
+        public static int Main(String[] args)
+        {
+            HttpServer httpServer;
+            if (args.GetLength(0) > 0)
+            {
+                httpServer = new MyHttpServer(Convert.ToInt16(args[0]));
+            }
+            else
+            {
+                httpServer = new MyHttpServer(8080);
+            }
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+            return 0;
+        }
+
+    }
+
+}
+
+
+
